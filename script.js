c03d03acce711db13574b4df01b4da56af871f6a
saurabh daswant <77434364+Saurabhdaswant@users.noreply.github.com>
2021-06-11 16:57:05 +0530

    Update script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+//grab the required stuff / input
+const coding = document.querySelector(".coding");
+const motivation = document.querySelector(".motivation");
+const fitness = document.querySelector(".fitness");
+const recomendationOne = document.querySelector(".recomendation--one");
+const recomendationTwo = document.querySelector(".recomendation--two");
+
+//processing
+function showCoding() {
+  recomendationOne.textContent = "CodeWithHarry";
+  recomendationTwo.textContent = "tanay pratap";
+}
+function showMotivation() {
+  recomendationOne.textContent = "beerBiceps";
+  recomendationOne.href =
+    "https://www.youtube.com/channel/UCPxMZIFE856tbTfdkdjzTSQ";
+  recomendationTwo.textContent = "aman dhattarwal";
+  recomendationTwo.href =
+    "https://www.youtube.com/channel/UCmXZxX_qexEZxhb5_vQKPCw";
+}
+function showFitness() {
+  recomendationOne.textContent = "saket ghokhale";
+  recomendationOne.href =
+    "https://www.youtube.com/channel/UCfgrg0SXgNkZ7rTbnZCp6tg";
+  recomendationTwo.textContent = "Fit Tuber";
+  recomendationTwo.href =
+    "https://www.youtube.com/channel/UCYC6Vcczj8v-Y5OgpEJTFBw";
+}
+
+//add event listeners / output
+coding.addEventListener("click", showCoding);
+motivation.addEventListener("click", showMotivation);
+fitness.addEventListener("click", showFitness);
